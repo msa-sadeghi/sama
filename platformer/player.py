0277413aaeda5e95d1810fba4ffad94cf3c0ee6f
msa-sadeghi <sadeghi.msai@gmail.com>
2024-04-21 18:58:28 +0330

    a

--- platformer/player.py
+++ platformer/player.py
@@ -16,23 +16,36 @@ class Player(Sprite):
         self.rect = self.image.get_rect()
         self.rect.topleft = (x,y)
         self.yVel = 0
+        self.image_update_time = pygame.time.get_ticks()
     def update(self, tiles)    :
         dx = 0
         dy = 0
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            dx -= 5
+        if keys[pygame.K_RIGHT]:
+            dx += 5
         dy += self.yVel
         self.yVel += 1
-        
         for t in tiles:
             if t[1].colliderect(self.rect.x, self.rect.y + dy, self.rect.size[0], self.rect.size[1]):
                 self.yVel = 0
                 dy = t[1].top - self.rect.bottom
-        
-        
+            if t[1].colliderect(self.rect.x + dx, self.rect.y, self.rect.size[0], self.rect.size[1]):
+                dx = 0    
         self.rect.x += dx
         self.rect.y += dy
-        
+        self.animation()
     def draw(self, screen):
         screen.blit(self.image, self.rect)
 
-    
+    def animation(self):
+        if pygame.time.get_ticks() - self.image_update_time > 100:
+            self.image_number += 1
+            self.image_update_time = pygame.time.get_ticks()
+        if self.image_number >= len(self.right_images):
+            self.image_number = 0
+        self.image = self.right_images[self.image_number]
+        
+        
         
