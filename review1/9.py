daae2a429b78d081047044f863ed5212481ce42b
msa-sadeghi <sadeghi.msai@gmail.com>
2024-12-21 17:17:26 +0330

    first commit

---
+++ review1/9.py
@@ -0,0 +1,22 @@
+# from tkinter import * 
+
+# window = Tk()
+# main_menu = Menu(window)
+# file_menu = Menu(main_menu, tearoff=0)
+# file_menu.add_command(label="New")
+# file_menu.add_command(label="Open")
+# file_menu.add_command(label="Exit")
+# main_menu.add_cascade(menu=file_menu, label="File")
+
+# window.configure(menu=main_menu)
+
+# window.mainloop()
+
+from tkinter import *
+
+
+window = Tk()
+
+# TODO Explain the frame with userpass example
+
+window.mainloop()
