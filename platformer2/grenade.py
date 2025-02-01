d949fc44d33001c494f09f718974ebe1c931f4b3
msa-sadeghi <sadeghi.msai@gmail.com>
2025-02-01 19:39:42 +0330

    Update grenade.py

--- platformer2/grenade.py
+++ platformer2/grenade.py
@@ -7,12 +7,18 @@ class Grenade(Sprite):
         self.image = pygame.image.load("./assets/images/icons/grenade.png")
         self.rect = self.image.get_rect(topleft=(x,y))
         self.direction = direction
-        self.yspeed = 0
+        self.yspeed = -15
+        self.xspeed = 4
         group.add(self)
         
     def update(self):
         self.yspeed += 1
-        self.rect.x += 4 * self.direction
+        self.rect.x += self.xspeed * self.direction
+        
+        if self.rect.bottom + self.yspeed >= 300:
+            self.yspeed = 0
+            self.xspeed = 0
+        
         self.rect.y += self.yspeed
         
     
